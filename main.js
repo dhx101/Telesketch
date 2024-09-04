0616817336d7c8be5de261d68f933a11aab8f0f0
David Huang Xie <138037766+dhx101@users.noreply.github.com>
2024-09-05 01:07:09 +0200

    Cambio de comentarios

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const container = document.querySelector("#contenedor");
 
-// Function to create a grid
+// Funcion para el grid
 function createGrid(size) {
 	for (let i = 1; i <= size; i++) {
 		const divColumna = document.createElement("div");
 		divColumna.classList.add("grid", "columna");
 		container.appendChild(divColumna);
 		for (let j = 1; j <= size; j++) {
-			// Changed inner loop variable to 'j' to avoid conflict
 			const divFila = document.createElement("div");
 			divFila.classList.add("grid", "fila");
 			divColumna.appendChild(divFila);
@@ -15,7 +14,7 @@ function createGrid(size) {
 	}
 }
 
-// Function to delete the existing grid
+// Funcion para elimiar el grid
 function deleteCube() {
 	const grids = document.querySelectorAll(".columna");
 	console.log(grids.length);
@@ -24,16 +23,14 @@ function deleteCube() {
 	}
 }
 
-// Function to change the color of a grid element
+// Funcion para cambiar el color del grid
 function changeGrid(element, colorElegido) {
 	element.classList.remove("grey", "black", "red", "blue", "green", "yellow");
 	element.classList.add(colorElegido);
 }
 
-// Variables for managing the current color
 let color = "grey";
 
-// Event listeners for color buttons
 const ColorGrey = document.querySelector(".grey");
 ColorGrey.addEventListener("click", () => (color = "grey"));
 
@@ -52,14 +49,12 @@ ColorGreen.addEventListener("click", () => (color = "green"));
 const ColorYellow = document.querySelector(".yellow");
 ColorYellow.addEventListener("click", () => (color = "yellow"));
 
-// Function to set event listeners on grid cells
 function setGridEventListeners() {
 	const grid = document.querySelectorAll(".fila");
 
 	grid.forEach((fila) => {
 		let isMouseDown = false;
 
-		// Set mousedown and mouseup listeners on the window to handle drag painting
 		window.addEventListener("mousedown", () => {
 			isMouseDown = true;
 		});
@@ -67,7 +62,6 @@ function setGridEventListeners() {
 			isMouseDown = false;
 		});
 
-		// Add mouseover event to change grid color while dragging
 		fila.addEventListener("mouseover", () => {
 			if (isMouseDown) {
 				changeGrid(fila, color);
@@ -76,17 +70,14 @@ function setGridEventListeners() {
 	});
 }
 
-// Create the initial grid
+// Crear grid inicial
 createGrid(8);
 setGridEventListeners();
 
-// Input button event listener to create a new grid
 const inputBtn = document.querySelector(".sendBtn");
 const inputSize = document.querySelector("#size");
 inputBtn.addEventListener("click", () => {
-	// Delete the old grid and create a new one with the specified size
 	deleteCube();
 	createGrid(inputSize.value);
-	// Set event listeners on the new grid
 	setGridEventListeners();
 });
